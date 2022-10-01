6bc8d2be00f4547b775ebaa6c91e9823f9a1e6a5
Tulsi Prasad <tulsi.prasad50@gmail.com>
2022-10-01 10:27:58 +0530

    :bug: Redirect to home page on opening unwanted routes

--- pages/hospital/[slug]/booking/index.js
+++ pages/hospital/[slug]/booking/index.js
@@ -1,81 +1,16 @@
-import { useState, useEffect } from 'react'
-import styled from 'styled-components'
-import Link from 'next/link'
+import React, { useEffect } from 'react'
 import { useRouter } from 'next/router'
-import {
-  Card,
-  Image,
-  Text,
-  Badge,
-  Button,
-  Group,
-  Grid,
-  Center,
-  SimpleGrid,
-  TextInput,
-  NumberInput,
-  Radio,
-} from '@mantine/core'
-import { Calendar } from '@mantine/dates'
-import dayjs from 'dayjs'
-import { useForm } from '@mantine/form'
-import * as R from 'rambda'
-
-import { useStore } from '../../../../config/store'
-import {
-  Container,
-  Doctor,
-  BookContainer,
-  CustomButton,
-  CustomCalender,
-  CustomTime,
-  Slot,
-  CustomHospitalName,
-  CustomDepartment,
-  CustomDoctors,
-} from '../../../../styles/js/Booking.styles.js'
-
-// Pages
-import Department from './department'
 
 const Booking = () => {
   const router = useRouter()
-  // const query = router.query
-  // const [date, setDate] = useState(null)
-  // const [time, setTime] = useState('')
 
-  const slotBooking = useStore((state) => state.slotBooking)
-  const setSlotBooking = useStore((state) => state.setSlotBooking)
-  // const currentHospital = useStore((state) => state.currentHospital)
-  // const setCurrentHospital = useStore((state) => state.setCurrentHospital)
+  // This route doesn't need to be displayed
+  // Redirect to home page
+  useEffect(() => {
+    router.push(`/`)
+  })
 
-  // const form = useForm({
-  //   initialValues: {
-  //     name: '',
-  //     guardian: '',
-  //     age: 0,
-  //     address: '',
-  //     gender: '',
-  //   },
-  // })
-
-  /**
-   * slotBooking: {
-   *   department: "",
-   *   doctor: "",
-   *   dateTime: "",
-   *   details: {}
-   * }
-   */
-
-  // Fill form
-  // TODO: Probably should go to another route: ie /booking/fill-form etc
-  // if (query.time && query.date) {
-  //   return (
-
-  //   )
-
-  return <h1>Hello World</h1>
+  return <></>
 }
 
 export default Booking
